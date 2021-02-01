c8e6bfa80e64a44f1e3011698e98b1fb0c090d5e
EdmilsonESF <ef.edmilsonfonseca@gmail.com>
2021-02-01 11:59:48 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,64 @@
+import React, { useState } from 'react';
 import styled from 'styled-components'
+import { useRouter } from 'next/router'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
+import QuizContainer from '../src/components/QuizContainer';
+
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  const router = useRouter();
+  const [name, setName] = useState('')
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function(event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}>
+              <Input 
+                name="nomeDoUsuario"
+                onChange={(event) => setName(event.target.value)}
+                placeholder="Diz ai seu nome"/>
+              <Button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  )
 }
